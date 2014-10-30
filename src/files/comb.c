bcec5b0731708274a02e44b2f62bc91ba28f7079
Lucas Cardoso <mr.lucascardoso@gmail.com>
2014-10-30 18:24:00 -0300

    Criacao do projeto do compilador. Foi criado o arquivo da primeira questao em HLL (Utilizaremos C)

---
+++ src/files/comb.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int fat(int value);// Função responsavel pelo fatorial
+
+// Função principal
+int main(){
+    //Logica principal
+}
+
+int fat(int value) {
+    //Logica do fatorial
+}
+
+

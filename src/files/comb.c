d904767fe3b9e10bb594f528b895cbf470b91470
Thiago Melo <melo-mesquita@hotmail.com>
2014-11-01 00:58:46 -0200

    foi feito uma alteracao no main

--- src/files/comb.c
+++ src/files/comb.c
@@ -4,7 +4,34 @@ int fat(int value);// Função responsavel pelo fatorial
 
 // Função principal
 int main(){
-    printf("Oi boquinha");
+   Int n,s,aux,Res; //declarando as variaveis
+
+   printf("numero n"); // aqui é para digitar o numero de n
+
+   printf("numero s"); // aqui para digitar o numero de s
+
+   aux = n!/s!(n-s)! // aqui ta  fazendo o fatorial de n e s
+
+   Res=aux; // aqui pegou o resultado do aux e jogou na variavel res
+
+   if(s>n){         // aparti daqui foi feito as comparações necessarias para cada if
+
+   printf("Erro 1 ");
+
+   }else if(n<0 || s<0){
+
+   printf("Erro2");
+
+   } else if(n==s){
+
+   printf("Erro 3");
+
+   }else if(n==0 & s==0){
+
+   printf("Erro 4");
+
+   }
+
 }
 
 int fat(int value) {
@@ -16,5 +43,3 @@ int fat(int value) {
     }
     return value;
 }
-
-

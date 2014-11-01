79810bd7c6863a39856ad55592a68b3080751042
Lucas Cardoso <mr.lucascardoso@gmail.com>
2014-11-01 14:48:17 -0300

    Mudei tua logica de deteccao d erro, desta forma ele imprime a saida do erro em exit(erro) e retorna o erro

--- src/files/comb.c
+++ src/files/comb.c
@@ -4,39 +4,30 @@ int fat(int value);// Função responsavel pelo fatorial
 
 // Função principal
 int main(){
+    int n, s, RES;                 // declarando as variaveis
+    scanf("%d", &n);               // lendo variavel n
+    scanf("%d", &s);               // lendo variavel s
+    if(  n < 0 || s < 0 ) {        // caso n e/ou s seja menor que zero
+        printf("exit(%d)\n", 2);   // emite erro 2
+        return 2;
+    } if( n == 0 || s == 0) {      // caso n e/ou s seja igual a zero
+        printf("exit(%d)\n", 4);   // emite erro 4
+        return 4;
+    } if( s > n ) {                // caso s  seja maior que n
+        printf("exit(%d)\n", 1);   // emite erro 1
+        return 1;
+    } if( n == s ) {               // caso n for igual a s
+        printf("exit(%d)\n", 3);   // emite erro 3
+        return 3;
+    }
 
-   Int n,s,aux,Res; //declarando as variaveis
 
-   printf("numero n"); // aqui é para digitar o numero de n
-
-   printf("numero s"); // aqui para digitar o numero de s
-
-   aux = n!/s!(n-s)! // aqui ta  fazendo o fatorial de n e s
-
-   Res=aux; // aqui pegou o resultado do aux e jogou na variavel res
-
-   if(s>n){         // aparti daqui foi feito as comparações necessarias para cada if
-
-   printf("Erro 1 ");
-
-   }else if(n<0 || s<0){
-
-   printf("Erro 2");
-
-   } else if(n==s){
-
-   printf("Erro 3");
-
-   }else if(n==0 & s==0){
-
-   printf("Erro 4");
-
-   }
-
+    printf("exit(%d)\n", 0);
+    return 0;
 }
 
 int fat(int value) {
-    if (value < 1) return 0; // Caso value for menor que 1 retornará 0
+    if (value < 1) return 0;     // caso value for menor que 1 retornará 0
     int aux = value - 1;
 
     while(aux > 1) {
